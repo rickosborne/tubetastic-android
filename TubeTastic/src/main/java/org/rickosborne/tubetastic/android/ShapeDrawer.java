ea112e065f8c21ea5bf421fbf7c107f8cca394b3
Rick Osborne <rosborne@fullsail.com>
2013-07-08 02:57:23 -0400

    Tile arcs are close

--- TubeTastic/src/main/java/org/rickosborne/tubetastic/android/ShapeDrawer.java
+++ TubeTastic/src/main/java/org/rickosborne/tubetastic/android/ShapeDrawer.java
@@ -4,7 +4,146 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShapeDrawer {
+
+    public static final float TWOPI = 2f * (float) Math.PI;
+    public static final float HALFPI = 0.5f * (float) Math.PI;
+    public static final float EPSILON = 0.00001f;
+
+    public static class LineSegment {}
+
+    public static class LineSegmentLine extends LineSegment {
+        public float x1 = 0;
+        public float y1 = 0;
+        public float x2 = 0;
+        public float y2 = 0;
+        public LineSegmentLine(float x1, float y1, float x2, float y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+    }
+
+    public static class LineSegmentArc extends LineSegment {
+        public float x = 0;
+        public float y = 0;
+        public float radius = 0;
+        public float startAngle = 0;
+        public float endAngle = 0;
+        public LineSegmentArc(float x, float y, float radius, float startAngle, float endAngle) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.startAngle = startAngle;
+            this.endAngle = endAngle;
+        }
+    }
+
+    public static class LineSegmentCurve extends LineSegment {
+        public float x1 = 0;
+        public float y1 = 0;
+        public float cx1 = 0;
+        public float cy1 = 0;
+        public float cx2 = 0;
+        public float cy2 = 0;
+        public float x2 = 0;
+        public float y2 = 0;
+        public LineSegmentCurve(float x1, float y1, float cx1, float cy1, float cx2, float cy2, float x2, float y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.cx1 = cx1;
+            this.cy1 = cy1;
+            this.cx2 = cx2;
+            this.cy2 = cy2;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+    }
+
+    public static List<LineSegmentCurve> createCurvesFromArc(float x, float y, float radius, float startAngle, float endAngle) {
+        // adapted from code by Hans Muller at:
+        // http://hansmuller-flex.blogspot.com/2011/10/more-about-approximating-circular-arcs.html
+        // normalize startAngle, endAngle to [-2PI, 2PI]
+        float startAngleN = startAngle % TWOPI;
+        float endAngleN = endAngle % TWOPI;
+        // Compute the sequence of arc curves, up to PI/2 at a time.  Total arc angle
+        // is less than 2PI.
+        ArrayList<LineSegmentCurve> curves = new ArrayList<LineSegmentCurve>();
+        float sgn = (startAngle < endAngle) ? +1 : -1; // clockwise or counterclockwise
+        float a1 = startAngle;
+        for (float totalAngle = Math.min(TWOPI, Math.abs(endAngleN - startAngleN)); totalAngle > EPSILON; )
+        {
+            float a2 = a1 + sgn * Math.min(totalAngle, HALFPI);
+            curves.add(createCurveFromArc(x, y, radius, a1, a2));
+            totalAngle -= Math.abs(a2 - a1);
+            a1 = a2;
+        }
+
+        return curves;
+    }
+
+    public static LineSegmentCurve createCurveFromArc(float x, float y, float radius, float startAngle, float endAngle) {
+        // Compute all four points for an arc that subtends the same total angle
+        // but is centered on the X-axis
+
+        float a = (endAngle - startAngle) / 2.0f;
+
+        float x4 = radius * (float) Math.cos(a);
+        float y4 = radius * (float) Math.sin(a);
+        float x1 = x4;
+        float y1 = -y4;
+
+        float q1 = (x1 * x1) + (y1 * y1);
+        float q2 = q1 + (x1 * x4) + (y1 * y4);
+        float k2 = 4f/3f * ((float) Math.sqrt(2 * q1 * q2) - q2) / ((x1 * y4) - (y1 * x4));
+
+        float x2 = x1 - (k2 * y1);
+        float y2 = y1 + (k2 * x1);
+        float x3 = x2;
+        float y3 = -y2;
+
+        // Find the arc points' actual locations by computing x1,y1 and x4,y4
+        // and rotating the control points by a + a1
+
+        float ar = a + startAngle;
+        float cos_ar = (float) Math.cos(ar);
+        float sin_ar = (float) Math.sin(ar);
+
+        return new LineSegmentCurve(
+            x + radius * (float) Math.cos(startAngle), y + radius * (float) Math.sin(startAngle),
+            x + x2 * cos_ar - y2 * sin_ar, y + x2 * sin_ar + y2 * cos_ar,
+            x + x3 * cos_ar - y3 * sin_ar, y + x3 * sin_ar + y3 * cos_ar,
+            x + radius * (float) Math.cos(endAngle), y + radius * (float) Math.sin(endAngle)
+        );
+    }
+
+    public static void renderLineSegments(ShapeRenderer shape, List<LineSegmentLine> segments, Color color, float lineWidth) {
+        shape.begin(ShapeRenderer.ShapeType.Line);
+        shape.identity();
+        shape.setColor(color);
+        Gdx.gl.glLineWidth(lineWidth);
+        for (LineSegmentLine l : segments) {
+            shape.line(l.x1, l.y1, l.x2, l.y2);
+        }
+        shape.end();
+    }
+
+    public static void renderArcSegments(ShapeRenderer shape, List<LineSegmentArc> segments, Color color, float lineWidth) {
+        shape.begin(ShapeRenderer.ShapeType.Curve);
+        shape.identity();
+        shape.setColor(color);
+        Gdx.gl.glLineWidth(lineWidth);
+        for (LineSegmentArc a : segments) {
+            for (LineSegmentCurve c : createCurvesFromArc(a.x, a.y, a.radius, a.startAngle, a.endAngle)) {
+                shape.curve(c.x1, c.y1, c.cx1, c.cy1, c.cx2, c.cy2, c.x2, c.y2);
+            }
+        }
+        shape.end();
+    }
 
     public static void line(ShapeRenderer shape, float x1, float y1, float x2, float y2, float lineWidth, Color color) {
         Gdx.gl.glLineWidth(lineWidth);
--- TubeTastic/src/main/java/org/rickosborne/tubetastic/android/TubeTile.java
+++ TubeTastic/src/main/java/org/rickosborne/tubetastic/android/TubeTile.java
@@ -6,7 +6,11 @@ import aurelienribon.tweenengine.TweenCallback;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+import java.util.ArrayList;
+
 public class TubeTile extends BaseTile {
+
+    public static final float DEGREES_TO_RADIANS = (float) Math.PI / 180f;
 
     private static final class OutletProbability {
         public double probability;
@@ -23,7 +27,55 @@ public class TubeTile extends BaseTile {
         }
     }
 
+    private static class OutletPath {
+        public int fromBit = 0;
+        public int toBit   = 0;
+        public OutletPath(int fromBit, int toBit) {
+            this.fromBit = fromBit;
+            this.toBit = toBit;
+        }
+    }
+
+    private static class OutletPathLine extends OutletPath {
+        private float x1;
+        private float y1;
+        private float x2;
+        private float y2;
+        public OutletPathLine(int fromBit, int toBit, float x1, float y1, float x2, float y2) {
+            super(fromBit, toBit);
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public ShapeDrawer.LineSegmentLine segment(float size, float cx, float cy) {
+            return new ShapeDrawer.LineSegmentLine(cx + size * x1, cy + size * y1, cx + size * x2, cy + size * y2);
+        }
+    }
+
+    public static class OutletPathArc extends OutletPath {
+        private float x;
+        private float y;
+        private float r;
+        private float a1;
+        private float a2;
+        public OutletPathArc(int fromBit, int toBit, float x, float y, float r, float startDegrees, float endDegrees) {
+            super(fromBit, toBit);
+            this.x = x;
+            this.y = y;
+            this.r = r;
+            this.a1 = startDegrees * DEGREES_TO_RADIANS;
+            this.a2 = endDegrees * DEGREES_TO_RADIANS;
+        }
+        public ShapeDrawer.LineSegmentArc segment(float size, float cx, float cy) {
+            return new ShapeDrawer.LineSegmentArc(cx + size * x, cy + size * y, size * r, a1, a2);
+        }
+    }
+
     private static final OutletProbability[] outletProbabilities;
+    private static final OutletPathLine[] outletPathLines;
+    private static final OutletPathArc[] outletPathArcs;
+    public static final float OFFSET_SINGLE = 0.25f;
     public static final float DURATION_VANISH = 0.500f;
     public static final float DURATION_DROP   = 0.250f;
     public static final float DURATION_SPIN   = 0.150f;
@@ -51,6 +103,18 @@ public class TubeTile extends BaseTile {
             Outlets.BIT_SOUTH,
             Outlets.BIT_WEST
         });
+        outletPathLines = new OutletPathLine[6];
+        outletPathLines[0] = new OutletPathLine(Outlets.BIT_NORTH, Outlets.BIT_SOUTH, 0, -1, 0, 1);
+        outletPathLines[1] = new OutletPathLine(Outlets.BIT_EAST , Outlets.BIT_WEST , -1, 0, 1, 0);
+        outletPathLines[2] = new OutletPathLine(0, Outlets.BIT_NORTH, 0, 1, 0, OFFSET_SINGLE);
+        outletPathLines[3] = new OutletPathLine(0, Outlets.BIT_EAST , 1, 0, OFFSET_SINGLE, 0);
+        outletPathLines[4] = new OutletPathLine(0, Outlets.BIT_SOUTH, 0, -1, 0, -OFFSET_SINGLE);
+        outletPathLines[5] = new OutletPathLine(0, Outlets.BIT_WEST , -1, 0, -OFFSET_SINGLE, 0);
+        outletPathArcs = new OutletPathArc[4];
+        outletPathArcs[0] = new OutletPathArc(Outlets.BIT_NORTH, Outlets.BIT_EAST ,  1,  1, 1, DEGREES_SOUTH, DEGREES_WEST );
+        outletPathArcs[1] = new OutletPathArc(Outlets.BIT_EAST,  Outlets.BIT_SOUTH,  1, -1, 1, DEGREES_WEST , DEGREES_NORTH);
+        outletPathArcs[2] = new OutletPathArc(Outlets.BIT_SOUTH, Outlets.BIT_WEST,  -1, -1, 1, DEGREES_NORTH, DEGREES_EAST);
+        outletPathArcs[3] = new OutletPathArc(Outlets.BIT_WEST , Outlets.BIT_NORTH, -1,  1, 1, DEGREES_EAST,  DEGREES_SOUTH);
     }
 
     private int spinRemain = 0;
@@ -190,26 +254,27 @@ public class TubeTile extends BaseTile {
 
     public void draw(ShapeRenderer shape) {
         ShapeDrawer.roundRect(shape, x + padding, y + padding, size - (2 * padding), size - (2 * padding), padding * 2, arcShadow(power));
-//        float cornerRadius = padding * 2;
-//        float cornerPoint = midpoint - (2 * cornerRadius);
-//        shape.begin(ShapeRenderer.ShapeType.FilledCircle);
-//        shape.identity();
-//        shape.translate(x + midpoint, y + midpoint, 0);
-//        shape.setColor(arcShadow(power));
-//        shape.filledCircle(-cornerPoint, cornerPoint, cornerRadius);
-//        shape.filledCircle(-cornerPoint, -cornerPoint, cornerRadius);
-//        shape.filledCircle(cornerPoint, cornerPoint, cornerRadius);
-//        shape.filledCircle(cornerPoint, -cornerPoint, cornerRadius);
-//        shape.end();
-//        shape.begin(ShapeRenderer.ShapeType.FilledRectangle);
-//        shape.identity();
-//        shape.translate(x + midpoint, y + midpoint, 0);
-//        shape.setColor(arcShadow(power));
-//        shape.filledRect(-cornerPoint - cornerRadius, -cornerPoint, cornerRadius, cornerPoint * 2);
-//        shape.filledRect(-cornerPoint, -cornerPoint - cornerRadius, cornerPoint * 2, (cornerPoint + cornerRadius) * 2);
-//        shape.filledRect(cornerPoint, -cornerPoint, cornerRadius, cornerPoint * 2);
-//        shape.end();
-//        Gdx.gl20.glLineWidth(arcWidth);
+        ArrayList<ShapeDrawer.LineSegmentLine> lines = new ArrayList<ShapeDrawer.LineSegmentLine>(outletPathLines.length);
+        float midX = x + midpoint;
+        float midY = y + midpoint;
+        int bits = outlets.getBits();
+        for (OutletPathLine line : outletPathLines) {
+            if (((line.fromBit == 0) && (bits == line.toBit)) || (((bits & line.fromBit) != 0) && ((bits & line.toBit) != 0))) {
+                lines.add(line.segment(midpoint, midX, midY));
+            }
+        }
+        if (lines.size() > 0) {
+            ShapeDrawer.renderLineSegments(shape, lines, COLOR_ARC, arcWidth);
+        }
+        ArrayList<ShapeDrawer.LineSegmentArc> arcs = new ArrayList<ShapeDrawer.LineSegmentArc>(outletPathArcs.length);
+        for (OutletPathArc arc : outletPathArcs) {
+            if (((bits & arc.fromBit) != 0) && ((bits & arc.toBit) != 0)) {
+                arcs.add(arc.segment(midpoint, midX, midY));
+            }
+        }
+        if (arcs.size() > 0) {
+            ShapeDrawer.renderArcSegments(shape, arcs, COLOR_ARC, arcWidth);
+        }
     }
 
 }
